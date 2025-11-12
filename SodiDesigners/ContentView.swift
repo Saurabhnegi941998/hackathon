b7ae90263a1de0b9d3a7c85a64771eaa93e36273
Saurabh.negi <saurabh.negi@falabella.cl>
2025-11-12 12:10:50 +0530

    Initial Commit

---
+++ SodiDesigners/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SodiDesigners
+//
+//  Created by Saurabh Negi on 12/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SodiDesigners/SodiDesignersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SodiDesignersApp.swift
+//  SodiDesigners
+//
+//  Created by Saurabh Negi on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SodiDesignersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
